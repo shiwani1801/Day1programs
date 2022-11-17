fe0275137eb5eaace1394dca624572d94d7ccec5
shiwani <shiwanipaunikar1@gmail.com>
2022-11-17 12:06:26 -0800

    [shiwani]added premitive data types

---
+++ src/com/bridgelabz/DefaultValueOfPremitiveDataType.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+public class DefaultValueOfPremitiveDataType {
+    static int i;
+    static byte b;
+    static long l;
+    static short s;
+    static char c;
+    static float f;
+    static double d;
+    static boolean bl;
+
+    public static void main(String[] args) {
+        System.out.println("The Default values of all primitive data types are:");
+        System.out.println("int :" + i);
+        System.out.println("byte :" + b);
+        System.out.println("long :" + l);
+        System.out.println("short :" + s);
+        System.out.println("char:" + c);
+        System.out.println("float :" + f);
+        System.out.println("double :" + d);
+        System.out.println("boolean :" + bl);
+
+    }
+}
+
