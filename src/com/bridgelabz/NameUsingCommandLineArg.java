0993717c8984406767295f2c5ab2bf9a14b97d26
shiwani <shiwanipaunikar1@gmail.com>
2022-11-17 12:33:07 -0800

    [shiwani]added leap year program

---
+++ src/com/bridgelabz/NameUsingCommandLineArg.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class NameUsingCommandLineArg {
+    public static void main(String[] args) {
+        String arg1 = args[0];
+        System.out.println(arg1);
+    }
+    }
+
