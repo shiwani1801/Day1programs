d9c6731dda62796ee296c29b249dc9c397a44592
shiwani <shiwanipaunikar1@gmail.com>
2022-11-17 12:09:35 -0800

    [shiwani]added two strings are equal program

---
+++ src/com/bridgelabz/TwoStringsEqual.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class TwoStringsEqual {
+    public static void main(String[] args) {
+        String a = "shiwani";
+        String b = "shivani";
+        // if(a==b){
+        //    System.out.println("a is equals to b");
+        //  }else {
+        //     System.out.println("a is not equals to b");
+        //  }
+
+
+
+        System.out.println( a.equals(b));
+    }
+
+
+}
